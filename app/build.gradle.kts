571c24c43d194f605a9fd0226bbb0e56373c45e9
jinsthomas30 <jinsthomas1992@gmail.com>
2025-05-20 08:53:10 +0100

    feat: Add dependencies for Hilt, Retrofit, Glide, and Kotlin Parcelize
    This commit adds the following libraries and plugins to the project:
    
    - **Hilt**: For dependency injection. Includes `hilt-android`, `hilt-compiler`, and `androidx-hilt-navigation-compose`.
    - **Retrofit**: For network calls. Includes `retrofit` and `converter-gson`.
    - **Glide**: For image loading.
    - **Kotlin Parcelize**: Plugin for generating Parcelable implementations.
    - **Kotlin KSP**: Kotlin Symbol Processing API, required by Hilt.
    
    Corresponding versions have been added to `gradle/libs.versions.toml` and plugin aliases have been configured in `build.gradle.kts` and `app/build.gradle.kts`.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,9 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.kotlinAndroidKsp)
+    alias(libs.plugins.hiltAndroid)
+    id("kotlin-parcelize")
 }
 
 android {
@@ -56,4 +59,15 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+    //Retrofit using network calling
+    implementation(libs.retrofit)
+    //GSON using network conversion
+    implementation(libs.converter.gson)
+    //Glide using image loading
+    implementation(libs.glide)
+    //Hilt using dependency injection
+    implementation(libs.hilt.android)
+    ksp(libs.hilt.compiler)
+    implementation(libs.androidx.hilt.navigation.compose)
 }
